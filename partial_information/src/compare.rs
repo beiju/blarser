1a6b6cfac2b82489b0ce993f5057da7b07481338
beiju <beiju@me.com>
2022-02-02 04:56:24 -0500

    Whoops I forgot the actual crates

--- partial_information/src/compare.rs
+++ partial_information/src/compare.rs
@@ -8,35 +8,35 @@ pub trait PartialInformationCompare {
 }
 
 pub trait PartialInformationFieldCompare {
-    fn get_conflicts(field_path: String, expected: &Self, actual: &Self) -> Vec<String>;
+    fn get_conflicts(field_path: String, expected: &Self, observed: &Self) -> Vec<String>;
 }
 
 impl<T> PartialInformationFieldCompare for T where T: PartialInformationCompare {
-    fn get_conflicts(field_path: String, expected: &Self, actual: &Self) -> Vec<String> {
-        expected.get_conflicts(actual).into_iter()
+    fn get_conflicts(field_path: String, expected: &Self, observed: &Self) -> Vec<String> {
+        expected.get_conflicts(observed).into_iter()
             .map(|s| format!("{}/{}", field_path, s))
             .collect()
     }
 }
 
 impl<ItemT> PartialInformationFieldCompare for Option<ItemT> where ItemT: PartialInformationFieldCompare + Debug {
-    fn get_conflicts(field_path: String, expected: &Self, actual: &Self) -> Vec<String> {
-        match (expected, actual) {
+    fn get_conflicts(field_path: String, expected: &Self, observed: &Self) -> Vec<String> {
+        match (expected, observed) {
             (None, None) => vec![],
-            (None, Some(val)) => vec![format!("{} Expected null, but value was {:?}", field_path, val)],
-            (Some(val), None) => vec![format!("{} Expected {:?}, but value was null", field_path, val)],
+            (None, Some(val)) => vec![format!("{} Expected null, but observed {:?}", field_path, val)],
+            (Some(val), None) => vec![format!("{} Expected {:?}, but observed null", field_path, val)],
             (Some(a), Some(b)) => PartialInformationFieldCompare::get_conflicts(field_path, a, b)
         }
     }
 }
 
 impl<ItemT> PartialInformationFieldCompare for Vec<ItemT> where ItemT: PartialInformationFieldCompare {
-    fn get_conflicts(field_path: String, expected: &Self, actual: &Self) -> Vec<String> {
-        if expected.len() != actual.len() {
-            vec![format!("{}: Expected length {}, but length was {}", field_path, expected.len(), actual.len())];
+    fn get_conflicts(field_path: String, expected: &Self, observed: &Self) -> Vec<String> {
+        if expected.len() != observed.len() {
+            vec![format!("{}: Expected length was {}, but observed length was {}", field_path, expected.len(), observed.len())];
         }
 
-        Iterator::zip(expected.iter(), actual.iter())
+        Iterator::zip(expected.iter(), observed.iter())
             .enumerate()
             .map(|(i, (a, b))| {
                 PartialInformationFieldCompare::get_conflicts(format!("{}/{}", field_path, i), a, b)
@@ -50,12 +50,12 @@ impl<ItemT> PartialInformationFieldCompare for Vec<ItemT> where ItemT: PartialIn
 macro_rules! trivial_compare {
     ($($t:ty),+) => {
         $(impl PartialInformationFieldCompare for $t {
-            fn get_conflicts(field_path: String, expected: &Self, actual: &Self) -> Vec<String> {
-                if actual.eq(expected) {
+            fn get_conflicts(field_path: String, expected: &Self, observed: &Self) -> Vec<String> {
+                if observed.eq(expected) {
                     return vec![];
                 }
 
-                vec![format!("{}: Expected {:?}, but value was {:?}", field_path, expected, actual)]
+                vec![format!("{}: Expected {:?}, but observed {:?}", field_path, expected, observed)]
             }
         })+
     }
---
+++ partial_information/src/lib.rs
@@ -0,0 +1,5 @@
+mod primitives;
+mod compare;
+
+pub use primitives::{Ranged};
+pub use compare::{PartialInformationFieldCompare, PartialInformationCompare};
